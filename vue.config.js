6b80df53600ada34cd388d657102b9afd0a5ea74
reda97dz <rbaka79@gmail.com>
2022-04-09 18:49:47 +0100

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
+module.exports = {
   transpileDependencies: ["vuetify"],
   publicPath: process.env.NODE_ENV === "production" ? "/punkapi_vue/" : "/",
-});
+};
